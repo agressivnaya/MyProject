1faaec44794b8b92a9c9d54abf9a7d4e185171ea
Irina Shiferson <st027925@student.spbu.ru>
2024-11-13 16:26:53 +0000

    Task 2 done

--- task2.py
+++ task2.py
@@ -1,5 +1,6 @@
-"""This is my file.
-But put here your code please
-instead of this text
-and send me a pull request
-so I will be able to review your code"""
+def power_check(n, b):
+    if n == 1:
+        return True
+    if n < b or n % b != 0:
+        return False
+    return power_check(n // b, b)
